e03928629163307b836edd77d5bc3e2816c42168
quan2003 <luuquan232003@gmail.com>
2025-05-15 11:40:52 +0700

    edit

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -11,14 +12,16 @@ app.use(express.json());
 
 const upload = multer({ storage: multer.memoryStorage() });
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 // Cấu hình PostgreSQL
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL.includes("localhost")
-    ? false
-    : { rejectUnauthorized: false },
+  ssl:
+    process.env.DATABASE_URL && process.env.DATABASE_URL.includes("localhost")
+      ? false
+      : { rejectUnauthorized: false },
 });
-
 // Thêm dữ liệu mặc định khi khởi động server
 const initializeDefaultData = async () => {
   try {
