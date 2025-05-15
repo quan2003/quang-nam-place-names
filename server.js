56342f76c063a970b92624f35494f651ff343495
quan2003 <luuquan232003@gmail.com>
2025-05-15 18:14:49 +0700

    Cập nhật CORS để khắc phục Network Error

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const fs = require("fs");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: "https://quang-nam-place-names-production.up.railway.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Phục vụ file tĩnh từ thư mục dist (frontend)
