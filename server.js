8e26020424010e7edbd379e3b883eb6d2342c480
quan2003 <luuquan232003@gmail.com>
2025-05-15 18:40:12 +0700

    Update

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"],
 };
+app.use(cors());
 app.use(cors(corsOptions));
 app.use(express.json());
 
