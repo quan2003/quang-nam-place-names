b2f4a57e0e47ec3c7c5c30207238512bcfb6b824
quan2003 <luuquan232003@gmail.com>
2025-05-15 11:53:32 +0700

    edit

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-require("dotenv").config(); // Nạp file .env cho môi trường cục bộ
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
